b98423433b2da967b6039b395a50417c4e030e99
jabezinu <jabezzinu31@gmail.com>
2025-06-09 01:21:21 +0300

    add navbar in to the route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,12 @@ import Menu from './pages/Menu'
 import About from './pages/About'
 import Home from './pages/Home'
 import Contact from './pages/Contact'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Menu />} />
         <Route path="/home" element={<Home />} />
