57174a7d7c9cda27544f9a36e1507004d3360bf1
jabezinu <jabezzinu31@gmail.com>
2025-06-09 03:56:21 +0300

    initialize server with Express and MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import employeeRoutes from './routes/employeeRoutes.js';
+import connectDB from './config/db.js';
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+
+// Employee API routes
+app.use('/api/employees', employeeRoutes);
+
+// MongoDB connection
+const PORT = process.env.PORT || 5000;
+
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    })
+    .catch((err) => {
+        console.error('MongoDB connection error:', err);
+    });
