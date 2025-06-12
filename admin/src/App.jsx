1d4bb757d6f491dba565e25ccf7a686cf3a6df94
jabezinu <jabezzinu31@gmail.com>
2025-06-12 22:30:36 +0300

    edit app

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,11 +3,14 @@ import  { Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar.jsx';
 import Menu from './pages/Menu.jsx';
 import Employee from './pages/Employee.jsx';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <div>
       <Sidebar />
+      <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
       <Routes>
         {/* <Route path="/" element={<Menu />} />
         <Route path="/employee" element={<Employee />} /> */}
