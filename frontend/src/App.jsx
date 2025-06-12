a37e190c5a4e99aaba41518e0f341e6d5043fdb1
jabezinu <jabezzinu31@gmail.com>
2025-06-12 22:31:39 +0300

    edit app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Menu from './pages/Menu'
 import About from './pages/About'
 import Home from './pages/Home'
 import Contact from './pages/Contact'
+import Register from './pages/Register'
+import Login from './pages/Login'
 import Navbar from './components/Navbar'
 
 const App = () => {
@@ -15,6 +17,8 @@ const App = () => {
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   )
