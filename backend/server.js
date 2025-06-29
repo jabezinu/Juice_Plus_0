871e3ac4048c97abf7a9f3ace91fbe27e6f2566b
jabezinu <jabezzinu31@gmail.com>
2025-06-29 09:48:46 +0300

    fix

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,17 @@ import dotenv from 'dotenv';
 import employeeRoutes from './routes/employeeRoutes.js';
 import menuRoutes from './routes/menuRoutes.js';
 import categoryRoutes from './routes/categoryRoutes.js';
-import userRoutes from './routes/userRoutes.js';
+import ratingRoutes from './routes/ratingRoutes.js';
 import connectDB from './config/db.js';
 import cors from 'cors';
 
 // Load environment variables
 dotenv.config();
 
+// Initialize Express app
 const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -19,7 +22,7 @@ app.use(express.json());
 app.use('/api/employees', employeeRoutes);
 app.use('/api/menus', menuRoutes);
 app.use('/api/categories', categoryRoutes);
-app.use('/api/users', userRoutes);
+app.use('/api/rating', ratingRoutes);
 
 // MongoDB connection
 const PORT = process.env.PORT || 5000;
