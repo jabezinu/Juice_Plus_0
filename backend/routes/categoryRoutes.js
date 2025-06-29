0f9ed146529b5b6482c0bb7de5098cccf914baac
jabezinu <jabezzinu31@gmail.com>
2025-06-29 09:51:26 +0300

    fix

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -2,7 +2,6 @@ import express from 'express';
 import {
     createCategory,
     getCategories,
-    getCategoryById,
     updateCategory,
     deleteCategory
 } from '../controller/categoryController.js';
@@ -11,7 +10,6 @@ const router = express.Router();
 
 router.post('/', createCategory);
 router.get('/', getCategories);
-router.get('/:id', getCategoryById);
 router.put('/:id', updateCategory);
 router.delete('/:id', deleteCategory);
 
