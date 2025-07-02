89a825602a835eee1465e1ffa819cbbfa7261b3c
jabezinu <jabezzinu31@gmail.com>
2025-07-02 08:57:06 +0300

    add new route

--- Manager/src/App.jsx
+++ Manager/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Menu from './Pages/Menu'
 import Employee from './Pages/Employee'
 import Navbar from './Components/Navbar'
+import OutOfStock from './Pages/OutOfStock'
 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Menu />} />
         <Route path="/employee" element={<Employee />} />
+        <Route path="/out-of-stock" element={<OutOfStock />} />
         {/* Add more routes as needed */}
       </Routes>
     </div>
