2166c02565cf5de360d59c19cd5730c2c0447d26
jabezinu <jabezzinu31@gmail.com>
2025-07-05 03:13:36 +0300

    second frontend vercel fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': 'http://localhost:5001',
-    },
+    historyApiFallback: true,
   },
 })
